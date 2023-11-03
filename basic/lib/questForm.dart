7cfe250f9693e7735805d7495063d2733fb526d1
Lauro Santana Silva <devlauross@gmail.com>
2023-11-03 18:01:32 -0300

    questForm widget

---
+++ basic/lib/questForm.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class QuestFormState extends State<QuestForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _onValidate(GlobalKey<FormState> _formKey) {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(const SnackBar(content: Text("Sucesso")));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value == "") {
+                  return "input inválido";
+                }
+              },
+            ),
+            ElevatedButton(
+                onPressed: () => _onValidate(_formKey),
+                child: const Text("Submit"))
+          ],
+        ));
+  }
+}
+
+class QuestForm extends StatefulWidget {
+  @override
+  createState() {
+    return QuestFormState();
+  }
+}
