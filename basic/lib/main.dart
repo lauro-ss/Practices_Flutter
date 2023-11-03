61a6f6f3a9e240f68e5cb249fb2b2fde84247b01
Lauro Santana Silva <devlauross@gmail.com>
2023-11-03 00:34:55 -0300

    main widget from stateful to stateless

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -3,21 +3,14 @@ import 'package:flutter/material.dart';
 
 main() => runApp(const App());
 
-class _AppState extends State<App> {
+class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: const Text("App Title")),
             body: const Quests()));
-  }
-}
-
-class App extends StatefulWidget {
-  const App({super.key});
-
-  @override
-  createState() {
-    return _AppState();
   }
 }
