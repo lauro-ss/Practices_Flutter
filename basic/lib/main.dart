f9d2a6c8d8482b012e3b8616d55998102ac48ffb
Lauro Santana Silva <devlauross@gmail.com>
2023-11-05 11:51:43 -0300

    add buttons for guide

--- basic/lib/main.dart
+++ basic/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:basic/quest_builder.dart';
 import 'package:basic/quest_form.dart';
 import 'package:basic/quests.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,8 @@ import 'package:flutter/material.dart';
 main() => runApp(const App());
 
 class AppState extends State<App> {
+  Widget? wg;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +19,54 @@ class AppState extends State<App> {
               ),
               backgroundColor: Colors.amber,
             ),
-            body: const Column(children: [Quests(), QuestForm()])));
+            body: wg ??
+                Center(
+                  child: SizedBox(
+                    height: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              wg = const Quests();
+                            });
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(20),
+                            child: Column(
+                              children: [
+                                Icon(
+                                  Icons.format_list_bulleted,
+                                  size: 50,
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  "Quests",
+                                  style: TextStyle(fontSize: 28),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 150),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              wg = const QuestForm();
+                            });
+                          },
+                          child: const Column(
+                            children: [
+                              Icon(Icons.format_list_bulleted),
+                              Text("Quests")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )));
   }
 }
 
